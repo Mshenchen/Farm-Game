6d5a1065d02c6536ac72836561f61251ddd1c3ca
mashenchen <704951265@qq.com>
2022-08-16 20:55:41 +0800

    8.16

---
+++ Assets/Scripts/AStar/MovementStep.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace Measy.AStar
+{
+    public class MovementStep : MonoBehaviour
+    {
+        public string sceneName;
+        public int hour;
+        public int minute;
+        public int second;
+        public Vector2Int gridCoordinate;
+    }
+}
+
